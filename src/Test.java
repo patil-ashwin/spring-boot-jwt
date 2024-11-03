fef4ec71dc33865b6876eb8d0580c8c1f04ddca4
Ashwin Patil <p.ashwin@hotmail.com>
2024-11-03 22:25:01 +0530

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -214,3 +214,55 @@ public class MicrosoftAuthService {
         });
     }
 }
+
+package com.example.auth.client;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.io.entity.StringEntity;
+
+import java.io.IOException;
+import java.time.Duration;
+
+@Slf4j
+public class AuthClient {
+
+    private final CloseableHttpClient client;
+
+    public AuthClient(Duration connectionTimeout, Duration socketTimeout, Duration connectionRequestTimeout) {
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectTimeout((int) connectionTimeout.toMillis())
+                .setResponseTimeout((int) socketTimeout.toMillis())
+                .setConnectionRequestTimeout((int) connectionRequestTimeout.toMillis())
+                .build();
+
+        this.client = HttpClients.custom()
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+    }
+
+    public String sendPost(String url, String formData) throws IOException {
+        HttpPost request = new HttpPost(url);
+        request.setEntity(new StringEntity(formData, ContentType.APPLICATION_FORM_URLENCODED));
+
+        try (CloseableHttpResponse response = client.execute(request)) {
+            int statusCode = response.getCode();
+            String responseBody = response.getEntity() != null ? EntityUtils.toString(response.getEntity()) : null;
+
+            log.info("Received response: Status Code = {}, Body = {}", statusCode, responseBody);
+
+            if (statusCode >= 200 && statusCode < 300) {
+                return responseBody;
+            } else {
+                throw new IOException("Received non-success response: " + statusCode + " - " + responseBody);
+            }
+        }
+    }
+}
+
